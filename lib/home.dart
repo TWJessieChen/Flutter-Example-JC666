9449707aff982b3ea5fe331ef040fcb2c9bad486
b09302083 <b09302083@gmail.com>
2020-12-04 15:18:04 +0800

    1.Add support uni_links package.

---
+++ lib/home.dart
@@ -0,0 +1,119 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_dart_jc666/platform/bridge.dart';
+import 'package:flutter_dart_jc666/native/native.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String response = "";
+  String responseStream = "";
+  int oneNumber = 1000;
+  int twoNumber = 1;
+  int _counter = 0;
+  String str = "JC666";
+
+  //One run One add 1
+  void _incrementCounter() {
+    setState(() {
+      _counter = Native.intAdd(_counter, 1);
+    });
+  }
+
+  StreamSubscription<String> internetConnectivityStream;
+
+  @override
+  void initState() {
+    super.initState();
+    getMessageFromNative();
+
+    listenToNativeStream();
+  }
+
+  @override
+  void dispose() {
+    internetConnectivityStream?.cancel();
+    super.dispose();
+  }
+
+  void listenToNativeStream()  {
+    internetConnectivityStream = NativeBridge.listenToNativeEventChannel().listen(
+            (string) {
+          setState(() {
+            this.responseStream = string;
+          });
+        }
+    );
+  }
+
+  void changeInternetConnectivity() {
+    NativeBridge.changeInternetConnectivity();
+  }
+
+  // void changeInternetConnectivity() async{
+  //   // NativeBridge.changeInternetConnectivity();
+  //
+  //   String responseFromNative = await NativeBridge.changeInternetConnectivity();
+  //   setState(() {
+  //     this.responseStream = responseFromNative;
+  //   });
+  //
+  // }
+
+  void getMessageFromNative() async {
+    String responseFromNative = await NativeBridge.sayHiToNative();
+    setState(() {
+      this.response = responseFromNative;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'The message from native platform message channel is',
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              '$response',
+              style: Theme.of(context).textTheme.display1,
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              // 'Native index++ : ',
+              // textAlign: TextAlign.center,
+              //   "native double value = ${doubleAdd(oneNumber.toDouble(), twoNumber.toDouble())}"
+                "native reverse str = ${Native.reverseFunc(str, str.length)}"
+
+            ),
+            Text(
+              // responseStream ? "YES" : "No",
+              '$responseStream',
+              style: Theme.of(context).textTheme.display1,
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: changeInternetConnectivity
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_dart_jc666/platform/bridge.dart';
-import 'package:flutter_dart_jc666/native/native.dart';
+import 'package:flutter_dart_jc666/home.dart';
+import 'package:uni_links/uni_links.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,121 +16,26 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'JC666 Flutter Demo Example'),
+        home: StreamBuilder(
+          stream: getLinksStream(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) { // our app started by configured links
+              var uri = Uri.parse(snapshot.data);
+              var list = uri.queryParametersAll.entries
+                  .toList(); // we retrieve all query parameters , tzd://genius-team.com?product_id=1
+
+              return Text(list.map((f) => f.toString()).join('-'));
+              // we just print all //parameters but you can now do whatever you want, for example open //product details page.
+            } else {
+              // our app started normally
+              return MyHomePage(title: 'JC666 Flutter Demo Example');
+            }
+          },
+        )
+      // home: MyHomePage(title: 'JC666 Flutter Demo Example'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
 
-  final String title;
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  String response = "";
-  String responseStream = "";
-  int oneNumber = 1000;
-  int twoNumber = 1;
-  int _counter = 0;
-  String str = "JC666";
-
-  //One run One add 1
-  void _incrementCounter() {
-    setState(() {
-      _counter = Native.intAdd(_counter, 1);
-    });
-  }
-
-  StreamSubscription<String> internetConnectivityStream;
-
-  @override
-  void initState() {
-    super.initState();
-    getMessageFromNative();
-
-    listenToNativeStream();
-  }
-
-  @override
-  void dispose() {
-    internetConnectivityStream?.cancel();
-    super.dispose();
-  }
-
-  void listenToNativeStream()  {
-    internetConnectivityStream = NativeBridge.listenToNativeEventChannel().listen(
-            (string) {
-          setState(() {
-            this.responseStream = string;
-          });
-        }
-    );
-  }
-
-  void changeInternetConnectivity() {
-    NativeBridge.changeInternetConnectivity();
-  }
-
-  // void changeInternetConnectivity() async{
-  //   // NativeBridge.changeInternetConnectivity();
-  //
-  //   String responseFromNative = await NativeBridge.changeInternetConnectivity();
-  //   setState(() {
-  //     this.responseStream = responseFromNative;
-  //   });
-  //
-  // }
-
-  void getMessageFromNative() async {
-    String responseFromNative = await NativeBridge.sayHiToNative();
-    setState(() {
-      this.response = responseFromNative;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'The message from native platform message channel is',
-              textAlign: TextAlign.center,
-            ),
-            Text(
-              '$response',
-              style: Theme.of(context).textTheme.display1,
-              textAlign: TextAlign.center,
-            ),
-            Text(
-              // 'Native index++ : ',
-              // textAlign: TextAlign.center,
-              //   "native double value = ${doubleAdd(oneNumber.toDouble(), twoNumber.toDouble())}"
-                "native reverse str = ${Native.reverseFunc(str, str.length)}"
-
-            ),
-            Text(
-              // responseStream ? "YES" : "No",
-              '$responseStream',
-              style: Theme.of(context).textTheme.display1,
-              textAlign: TextAlign.center,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-          onPressed: changeInternetConnectivity
-      ),
-    );
-  }
-}
